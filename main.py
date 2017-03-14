8de8a96b952a8b6dd9aedc394204e78255c00ba3
Alex Mathson <mathsona@carleton.edu>
2017-03-13 22:34:48 -0500

    Commented code and added results.txt

--- main.py
+++ main.py
@@ -4,17 +4,26 @@ import csv
 import random
 import operator
 
+#These constants can be changed to switch the data sets used to train, tune, and
+#test a perceptron or two-layer neural network.
 TRAINING_FILE_NAME = "Data Set/trainingDataGeneric.csv"
 VERIFICATION_FILE_NAME = "Data Set/verificationDataGeneric.csv"
 TEST_FILE_NAME = "Data Set/testDataGeneric.csv"
 
+#These constants can be changed to alter the learning rate
+#and number of hidden nodes of a two-layer neural network.
 ALPHA = 0.05
 N_HIDDEN = 14
 
+
+#Calculates and returns the output of the sigmoid specified by
+#weights, with features as the input.
 def calculate_sigmoid_output(weights, features):
     sumScore = np.dot(weights, features)
     return float(1) / (1 + math.e**sumScore)
 
+#Calculates and returns the output of the perceptron defined by
+#weights, with features as the input.
 def calculate_perceptron_output(weights, features):
     sumScore = np.dot(weights, features)
     output = 1.0
@@ -22,12 +31,15 @@ def calculate_perceptron_output(weights, features):
         output = -1.0
     return output
 
+#Given a single training example and starting perceptron weights,
+#update and return the weights.
 def update_perceptron_weights(oldWeights, trainingExample):
     targetValue = trainingExample[1]
     newWeights = map(operator.add, oldWeights, np.dot(targetValue, trainingExample[0]))
     return newWeights
 
 
+#Reads filename and returns all training or test instances from that file.
 def getExamplesFromFile(fileName):
     with open(fileName, 'rU') as csvfile:
         trainingExamples = []
@@ -37,7 +49,9 @@ def getExamplesFromFile(fileName):
             trainingExamples.append(example)
         return trainingExamples
 
-
+#Propagates the feature values through the two-layer neural network
+#specified by hiddenWeights and outputWeights, and returns
+#both the outputs of the hidden layer and the outputs of the perceptron.
 def forwardPropagate(hiddenWeights, outputWeights, features):
     hiddenOutputs = []
     for hiddenWeight in hiddenWeights:
@@ -99,6 +113,8 @@ def stochastic_backpropagation(trainingExamples, alpha, n_hidden):
 
     return hidden_weights, output_weights
 
+#Calculates and returns the weights for a perceptron
+#trained on trainingExamples.
 def calculate_perceptron_weights(trainingExamples):
     weights = [0.0] * len(trainingExamples[0][0])
     for example in trainingExamples:
@@ -108,7 +124,8 @@ def calculate_perceptron_weights(trainingExamples):
             weights = np.array(weights) + np.dot(target_value, example[0])
     return weights
 
-
+#Classifies all instances withing testExamples using a perceptron specified
+#by weights, and returns the percent of instances correctly classified.
 def perceptron_classify(weights, testExamples):
     num = 0
     num_correct = 0
@@ -121,6 +138,8 @@ def perceptron_classify(weights, testExamples):
     percent_correct = float(num_correct) / num
     return percent_correct
 
+#Classfies all instances within testExamples using the two-layer neural network specified
+#by hidden_weights and output_weights, and returns the percent of instances correctly classified.
 def neural_net_classify(hidden_weights, output_weights, testExamples):
     num_correct = 0
     num = 0
@@ -133,15 +152,20 @@ def neural_net_classify(hidden_weights, output_weights, testExamples):
     percent_correct = float(num_correct) / num
     return percent_correct
 
-
+#Iterates through a range of values for both the number of hidden states and the learning rate
+#of a two-layer neural network, and prints the results of applying each of these neural networks
+#to the validation set along with their hyper-parameters to a text file. One of the top performing
+#neural networks will also be printed to the bottom of the file. Depending on the range of values allowed
+#for both the learning rate and the number of hidden nodes, this function require a significant amount
+#of time(i.e. hours) to finish running.
 def tune():
     max_correct = 0.0
     max_alpha = 0.0
     max_hidden = 0
     trainingExamples = getExamplesFromFile(TRAINING_FILE_NAME)
     verificationExamples = getExamplesFromFile(VERIFICATION_FILE_NAME)
-    resultsFile = open("results4.txt", 'w')
-    for n_hidden in range(25, 30):
+    resultsFile = open("results.txt", 'w')
+    for n_hidden in range(3, 30):
         for alpha in np.arange(.01, .20, .01):
             hidden_weights, output_weights = stochastic_backpropagation(trainingExamples, alpha, n_hidden)
             num = 0
@@ -164,6 +188,9 @@ def tune():
     resultsFile.write(maxString + '\n')
     resultsFile.close()
 
+#Trains a perceptron and a two-layer neural network specified by
+#N_HIDDEN and ALPHA on trainingExamples before appyling both neural networks
+#to both the validation set and the test set, and printing the results.
 def main():
     trainingExamples = getExamplesFromFile(TRAINING_FILE_NAME)
     verificationExamples = getExamplesFromFile(VERIFICATION_FILE_NAME)
